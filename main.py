c1fad7802eb86a6b8ce362160ba2bcc366d5cc81
meesam4687 <meesam4687@gmail.com>
2024-10-12 22:12:17 +0530

    feat: added cors

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from llm import createTT
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/generate', methods=['POST'])
 def process():
@@ -13,6 +14,4 @@ def process():
     return jsonify({'error': 'No prompt provided'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-    #i made no change
+    app.run()
